5ccbbf1320f522017a19b1175577b4bac350ff54
Muhammad Arslan <168574259+Arsal75@users.noreply.github.com>
2024-04-30 20:46:15 +0300

    Create SyntaxErrorJavascript.js

---
+++ Syntaxerrorofjs.js
@@ -0,0 +1,2 @@
+console.log("Hello, world!"
+//In this example, the closing parenthesis ")" is missing at the end of the console.log statement, which would cause a syntax error when the code is executed.
