ff69227d9687a6c90abe0365f9fdfcc71463631c
Muhammad Arslan <168574259+Arsal75@users.noreply.github.com>
2024-04-30 21:31:42 +0300

    Update Syntaxerrorofjs.js

--- Syntaxerrorofjs.js
+++ Syntaxerrorofjs.js
@@ -1,2 +1,36 @@
-console.log("Hello, world!"
-//In this example, the closing parenthesis ")" is missing at the end of the console.log statement, which would cause a syntax error when the code is executed.
+Description:
+
+This JavaScript code snippet demonstrates a common syntax error that developers might encounter. The missing closing parenthesis ")" at the end of the console.log statement results in a syntax error when the code is executed.
+
+```javascript
+console.log("Hello, world!";
+```
+
+Here are some more examples of syntax errors in JavaScript:
+
+1. Missing semicolon at the end of a statement:
+
+```javascript
+var x = 5
+```
+
+2. Undefined variable:
+
+```javascript
+console.log(y);
+```
+
+3. Missing closing curly brace:
+
+```javascript
+if (true) {
+    console.log("Hello");
+```
+
+4. Misspelled function name:
+
+```javascript
+consol.log("Hello");
+```
+
+These examples serve as reminders to carefully check syntax to ensure smooth execution of JavaScript programs.
